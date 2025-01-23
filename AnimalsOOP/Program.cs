61e3ff538da58183ca13e614bf103a60830eed23
Tim Widmoser <82330733+timwiddo@users.noreply.github.com>
2025-01-23 13:56:47 +0100

    Add files via upload

---
+++ AnimalsOOP/Program.cs
@@ -0,0 +1,59 @@
+﻿public interface IAnimal
+{
+    void Eat();
+}
+
+public class Animal : IAnimal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic animal sound");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("Some generic animal food");
+    }
+}
+
+public class Dog : Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Kibble");
+    }
+
+    public override void MakeSound(){
+        Console.WriteLine("Bark");
+    }
+}
+
+public class Cat : Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Tuna");
+    }
+
+    public override void MakeSound(){
+        Console.WriteLine("Meow");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Dog myDog = new Dog();
+        Cat myCat = new Cat();
+        
+        myDog.MakeSound(); // Should print "Bark"
+        myCat.MakeSound(); // Should print "Meow"
+
+        myDog.Eat(); // Should print "Kibble"
+        myCat.Eat(); // Should print "Tuna"
+
+    }
+}
+
+
