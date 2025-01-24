31047be4029e5734bf6f6f9443ca3443d3909bf7
Tim Widmoser <82330733+timwiddo@users.noreply.github.com>
2025-01-24 14:29:08 +0100

    Add files via upload

---
+++ AsyncPractice/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Program
+{
+public static async Task DownloadDataAsync()
+{
+    try
+    {
+        Console.WriteLine("Download started...");
+        throw new InvalidOperationException("Simulated download error.");
+        await Task.Delay(3000);
+        Console.WriteLine("Download completed.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("An error occurred: " + ex.Message);
+    }
+}
+
+public async Task DownloadDataAsync2()
+{
+    Console.WriteLine("Download 2 started...");
+    await Task.Delay(2000);
+    Console.WriteLine("Download 2 completed.");
+}
+
+
+public static async Task Main(string[] args)
+{
+    Program program = new Program();
+    Task task1 = DownloadDataAsync();
+    Task task2 = program.DownloadDataAsync2();
+    await Task.WhenAll(task1, task2);
+    Console.WriteLine("All downloads completed!");
+    Console.WriteLine("Main method completed!");
+}
+
+}
